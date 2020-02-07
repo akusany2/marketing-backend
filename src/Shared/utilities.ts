fe75dc5fb5d2802779b0cbb4e57726c4bac207a2
ankumar <ankumar@IND116>
2020-02-07 17:02:18 +0530

    Repalce string in utilities

--- src/Shared/utilities.ts
+++ src/Shared/utilities.ts
@@ -2,28 +2,39 @@ import * as bcrypt from 'bcrypt';
 import { passwordHash } from '../config';
 
 export async function hashPassword(password): Promise<string> {
-  const hashedPassword = await new Promise<string>((resolve, reject) => {
-    bcrypt.hash(password, passwordHash.saltRounds, (err, hash) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(hash);
-    });
-  });
+	const hashedPassword = await new Promise<string>((resolve, reject) => {
+		bcrypt.hash(password, passwordHash.saltRounds, (err, hash) => {
+			if (err) {
+				reject(err);
+			}
+			resolve(hash);
+		});
+	});
 
-  return hashedPassword;
+	return hashedPassword;
 }
 
 export async function comparePassword(
-  password,
-  hashedPassword,
+	password,
+	hashedPassword,
 ): Promise<string> {
-  return await new Promise<string>((resolve, reject) => {
-    bcrypt.compare(password, hashedPassword, (err, result) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(result);
-    });
-  });
+	return await new Promise<string>((resolve, reject) => {
+		bcrypt.compare(password, hashedPassword, (err, result) => {
+			if (err) {
+				reject(err);
+			}
+			resolve(result);
+		});
+	});
+}
+
+export function findReplaceString(string, find, replace) {
+	if (/[a-zA-Z\_]+/g.test(string)) {
+		return string.replace(
+			new RegExp('{{(?:\\s+)?(' + find + ')(?:\\s+)?}}'),
+			replace,
+		);
+	} else {
+		return string;
+	}
 }
